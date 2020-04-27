2df84e77065ca8a05d593838eb9b39e8e4253497
honnnei <hannpop@gmail.com>
2020-04-27 12:23:33 +0200

    working nav tests

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+const NavigationBar = (props) => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">QuizName</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/newgame/">New Game</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/pastscores">Past Scores</NavLink>
+            </NavItem>
+          </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavigationBar;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
+import Navbar from '../components/Navbar';
+import { shallow, mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  test('renders Navbar', () => {
+    expect(wrapper.containsMatchingElement(<Navbar />)).toEqual(true);
+  });
+
+  // test("render a button with text of 'increment'", () => {
+  //   expect(wrapper.find('#increment-btn').text()).toBe('Increment Me!');
+  // });
+
+  // test("render the initial value of state in a div", () => {
+  //   expect(wrapper.find("#counter-value").text()).toBe("0");
+  // });
+
+  // test("render the click event of increment button and increment counter value", () => {
+  //   wrapper.find('#increment-btn').simulate('click');
+  //   expect(wrapper.find('#counter-value').text()).toBe("1");
+  // });
+
 });
