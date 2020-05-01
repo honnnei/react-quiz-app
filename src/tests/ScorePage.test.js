d8a733b26a8cbe2ac5e375a58b6fdaa3e52ed417
honnnei <hannpop@gmail.com>
2020-05-01 15:37:35 +0200

    snapshot test for scorepage

--- src/tests/ScorePage.test.js
+++ src/tests/ScorePage.test.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter,  MemoryRouter, Link } from 'react-router-dom';
-import { shallow, mount } from 'enzyme'
-import ScorePage from '../containers/ScorePage'
+import { shallow, mount } from 'enzyme';
+import ScorePage from '../containers/ScorePage';
+import renderer from 'react-test-renderer';
 
 
 describe('ScorePage', () => {
+
+    it('matches the snapshot', () => {
+      const tree = renderer.create(<ScorePage location={{userScores: {1: 0}, state:{userScores: {1: 0}, stateFinal: {userNames: {0: "1"}}}}}/>).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
     it('should render scores-container div', () => {
         const wrapper = mount( <ScorePage location={{userScores: {1: 0}, state:{userScores: {1: 0}, stateFinal: {userNames: {0: "1"}}}}}/> )
         expect(wrapper.exists('.scores-container')).toEqual(true);
