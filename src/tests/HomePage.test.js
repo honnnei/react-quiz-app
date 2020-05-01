c3ca52669f5582cc82904f9c9997be3a682d60b8
honnnei <hannpop@gmail.com>
2020-05-01 15:56:36 +0200

    snapshot for HomePage

--- src/tests/HomePage.test.js
+++ src/tests/HomePage.test.js
@@ -1,16 +1,21 @@
-
-import React from 'react'
-import { shallow, mount } from 'enzyme'
-import HomePage from '../containers/HomePage'
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import HomePage from '../containers/HomePage';
 import renderer from 'react-test-renderer';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 
 describe('<HomePage />', () => {
 
+    it('matches the snapshot', () => {
+        const tree = renderer.create(<BrowserRouter><HomePage /></BrowserRouter>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     it('should render a Form componeent', () => {
         const wrapper = shallow(<HomePage  />); 
         expect(wrapper.exists('Form')).toEqual(true);
-    } )
+    });
 })
 
 
