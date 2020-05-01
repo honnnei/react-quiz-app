e67ccb35897595c49284b199ea28f2662aed1f8e
rorieArmstrong <rorie.armstrong@gmail.com>
2020-05-01 13:13:26 +0100

    fixed scorepage

--- src/containers/ScorePage.js
+++ src/containers/ScorePage.js
@@ -23,14 +23,14 @@ function ScorePage(props) {
                 });
         } else{console.log('no userscores found')} })
     
-        if(maxScores.length === 1 && scores.length >= 1 ) {
+        if(maxScores.length === 1 && scores.length > 1 ) {
             let sorted = scores.sort((a, b) =>{return b.score - a.score });
             winner = [sorted[0].user]
-        } else if(maxScores.length > 1 && scores.length >= 1) {
+        } else if(maxScores.length > 1 && scores.length > 1) {
             let result = scores.filter(item => item.score === max)
             winner = result.map(x => x.user)  
         } else{
-            console.log('not enough users')
+            winner = []
         }
 
         
@@ -38,7 +38,7 @@ function ScorePage(props) {
         <div className="scorepage-container">
             <h1>Final Scores</h1>
             <div className="scores-container">
-                {winner.length === 1 ? <h2>The winner is: {winner}</h2> : winner.length > 1 ? <h2>The winners are: {winner.join(', ')}</h2> : <h2> Good Job!</h2>  }
+                {winner.length === 1 ? <h2>The winner is: {winner[0]}</h2> : winner.length > 1 ? <h2>The winners are: {winner.join(', ')}</h2> : <h2> Good Job!</h2>  }
 
                     {(Object.values(props.location.state.stateFinal.userNames)).map(user => {
                         return (<h3 key={user}>{user}:&#160;&#160;&#160;{props.location.state.userScores[user]}</h3>)
